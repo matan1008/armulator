e156d91e59908f0dcd535dfd15112f792e9d1fc5
Matan Perelman <matan1008@gmail.com>
2017-05-18 21:11:11 +0300

    test for CMP immediate in T1 encoding added

--- tests/opcode_tests/test_arithmetic_thumb.py
+++ tests/opcode_tests/test_arithmetic_thumb.py
@@ -4,6 +4,7 @@ from armulator.shift import SRType
 from armulator.opcodes.thumb_instruction_set.thumb_instruction_set_encoding_16_bit.thumb_shift_immediate_add_subtract_move_and_compare.add_immediate_thumb_t1 import AddImmediateThumbT1
 from armulator.opcodes.thumb_instruction_set.thumb_instruction_set_encoding_16_bit.thumb_shift_immediate_add_subtract_move_and_compare.lsl_immediate_t1 import LslImmediateT1
 from armulator.opcodes.thumb_instruction_set.thumb_instruction_set_encoding_16_bit.thumb_shift_immediate_add_subtract_move_and_compare.add_register_thumb_t1 import AddRegisterThumbT1
+from armulator.opcodes.thumb_instruction_set.thumb_instruction_set_encoding_16_bit.thumb_shift_immediate_add_subtract_move_and_compare.cmp_immediate_t1 import CmpImmediateT1
 
 def test_add_immediate_thumb():
     arm = ARM1176()
@@ -63,4 +64,19 @@ def test_add_register_thumb():
     assert arm.core_registers.get_cpsr_z() == "0"
     assert arm.core_registers.get_cpsr_c() == "0"
     assert arm.core_registers.get_cpsr_v() == "1"
-    
+
+def test_cmp_immediate_thumb():
+    arm = ARM1176()
+    arm.take_reset()
+    instr = BitArray(bin="0010100000000101")
+    opcode = arm.decode_instruction(instr)
+    opcode = opcode.from_bitarray(instr, arm)
+    assert type(opcode) == CmpImmediateT1
+    assert opcode.n == 0
+    assert opcode.imm32 == BitArray(hex="0x00000005")
+    arm.core_registers.set(opcode.n, BitArray(hex="0x00000004"))
+    arm.execute_instruction(opcode)
+    assert arm.core_registers.get_cpsr_n() == "1"
+    assert arm.core_registers.get_cpsr_z() == "0"
+    assert arm.core_registers.get_cpsr_c() == "0"
+    assert arm.core_registers.get_cpsr_v() == "0"
