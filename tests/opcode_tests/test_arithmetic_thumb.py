92148931a96c71c13f9ab13c76aa6d16d83c9d3f
Matan Perelman <matan1008@gmail.com>
2017-05-18 20:42:13 +0300

    test for ADD register, T1 encoding added

--- tests/opcode_tests/test_arithmetic_thumb.py
+++ tests/opcode_tests/test_arithmetic_thumb.py
@@ -1,7 +1,9 @@
 from armulator.arm1176 import ARM1176
 from bitstring import BitArray
+from armulator.shift import SRType
 from armulator.opcodes.thumb_instruction_set.thumb_instruction_set_encoding_16_bit.thumb_shift_immediate_add_subtract_move_and_compare.add_immediate_thumb_t1 import AddImmediateThumbT1
 from armulator.opcodes.thumb_instruction_set.thumb_instruction_set_encoding_16_bit.thumb_shift_immediate_add_subtract_move_and_compare.lsl_immediate_t1 import LslImmediateT1
+from armulator.opcodes.thumb_instruction_set.thumb_instruction_set_encoding_16_bit.thumb_shift_immediate_add_subtract_move_and_compare.add_register_thumb_t1 import AddRegisterThumbT1
 
 def test_add_immediate_thumb():
     arm = ARM1176()
@@ -39,3 +41,26 @@ def test_lsl_immediate_thumb():
     assert arm.core_registers.get_cpsr_n() == "0"
     assert arm.core_registers.get_cpsr_z() == "0"
     assert arm.core_registers.get_cpsr_c() == "0"
+
+def test_add_register_thumb():
+    arm = ARM1176()
+    arm.take_reset()
+    instr = BitArray(bin="0001100001010011")
+    opcode = arm.decode_instruction(instr)
+    opcode = opcode.from_bitarray(instr, arm)
+    assert type(opcode) == AddRegisterThumbT1
+    assert opcode.setflags == True
+    assert opcode.m == 1
+    assert opcode.d == 3
+    assert opcode.n == 2
+    assert opcode.shift_n == 0
+    assert opcode.shift_t == SRType.SRType_LSL
+    arm.core_registers.set(opcode.n, BitArray(hex="0x70000000"))
+    arm.core_registers.set(opcode.m, BitArray(hex="0x10000001"))
+    arm.execute_instruction(opcode)
+    assert arm.core_registers.get(opcode.d) == BitArray(hex="0x80000001")
+    assert arm.core_registers.get_cpsr_n() == "1"
+    assert arm.core_registers.get_cpsr_z() == "0"
+    assert arm.core_registers.get_cpsr_c() == "0"
+    assert arm.core_registers.get_cpsr_v() == "1"
+    
