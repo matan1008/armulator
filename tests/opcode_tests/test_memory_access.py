1e49dac612f1c51faf6d54b4e0471a6488567f78
Matan Perelman <matan1008@gmail.com>
2017-05-19 16:30:16 +0300

    Adding tests from memory accessing opcodes

---
+++ tests/opcode_tests/test_memory_access.py
@@ -0,0 +1,30 @@
+from armulator.arm1176 import ARM1176
+from bitstring import BitArray
+from armulator.shift import SRType
+from armulator.memory_types import RAM
+from armulator.opcodes.thumb_instruction_set.thumb_instruction_set_encoding_16_bit.thumb_load_store_single_data_item.ldr_register_thumb_t1 import LdrRegisterThumbT1
+
+def test_ldr_register_thumb():
+    arm = ARM1176()
+    arm.take_reset()
+    instr = BitArray(bin="0101100001010011")
+    # setting Data Region registers
+    arm.core_registers.DRSRs[0][31] = True
+    arm.core_registers.DRSRs[0][26:31] = "0b00010"
+    arm.core_registers.DRBARs[0] = BitArray(hex="0x0F000000")
+    arm.core_registers.DRACRs[0] = BitArray(hex="0x00000300")
+    arm.core_registers.MPUIR = BitArray(hex="0x00010100")
+    ram_memory = RAM(0x100)
+    ram_memory.write(0x4, 4, "ECIN")
+    arm.mem.memories.append((ram_memory, (0x0F000000, 0x0F000100)))
+    opcode = arm.decode_instruction(instr)
+    opcode = opcode.from_bitarray(instr, arm)
+    assert type(opcode) == LdrRegisterThumbT1
+    assert opcode.m == 1
+    assert opcode.n == 2
+    assert opcode.t == 3
+    assert opcode.shift_n == 0
+    assert opcode.shift_t == SRType.SRType_LSL
+    arm.core_registers.set(opcode.n, BitArray(hex="0x0F000004"))
+    arm.execute_instruction(opcode)
+    assert arm.core_registers.get(opcode.t).bytes == "NICE"
