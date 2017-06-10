bd8d6c42bb09351743953941328fb8ca0da398f8
Matan Perelman <matan1008@gmail.com>
2017-06-10 21:37:19 +0300

    Memory types refactoring

--- armulator/memory_types.py
+++ armulator/memory_types.py
@@ -8,19 +8,17 @@ class MemoryType(object):
     def __init__(self, size):
         pass
 
-    @staticmethod
     def __getitem__(self, (address, size)):
-        pass
+        return self.read(address, size)
 
-    @abstractmethod
     def __setitem__(self, address_size, value):
-        pass
+        self.write(address_size[0], address_size[1], value)
 
     @abstractmethod
     def read(self, address, size):
         pass
 
-    @staticmethod
+    @abstractmethod
     def write(self, address, size, value):
         pass
 
@@ -30,18 +28,12 @@ class RAM(MemoryType):
         super(RAM, self).__init__(size)
         self.memory_array = bytearray(size)
 
-    def __getitem__(self, (address, size)):
+    def read(self, address, size):
         chunk = self.memory_array[address:address + size]
         return chunk
 
-    def __setitem__(self, address_size, value):
-        self.memory_array[address_size[0]:address_size[0] + address_size[1]] = value
-
-    def read(self, address, size):
-        return self[address, size]
-
     def write(self, address, size, value):
-        self.__setitem__((address, size), value)
+        self.memory_array[address:address + size] = value
 
 
 MEMORY_TYPE_DICT = {
