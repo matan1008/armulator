3315d3165dae57e826356afbf2434e8fc654f84a
Matan Perelman <matan1008@gmail.com>
2017-06-30 18:26:41 +0300

    more tests

--- tests/test_pc_incrementing.py
+++ tests/test_pc_incrementing.py
@@ -22,3 +22,24 @@ def test_increment_pc():
     assert not arm.registers.cpsr.get_v()
     assert arm.registers.pc_store_value().uint - prev_pc.uint == 2
     assert arm.registers.get(1) == BitArray(hex="0x00000013")
+
+
+def test_branching_t1():
+    arm = ArmV6()
+    arm.take_reset()
+    prev_pc = BitArray(hex="0x000514F6")
+    arm.registers.sctlr.set_m(False)
+    arm.registers.branch_to(prev_pc)
+    ram_memory = RAM(0x100)
+    ram_memory[0xd8, 2] = "\x41\x1c"
+    ram_memory[0xf6, 2] = "\xef\xdc"
+    mc = MemoryController(ram_memory, 0x00051400, 0x00051500)
+    arm.mem.memories.append(mc)
+    arm.registers.set(0, BitArray(hex="0x00000012"))
+    arm.registers.cpsr.set_n(False)
+    arm.registers.cpsr.set_z(False)
+    arm.registers.cpsr.set_c(False)
+    arm.registers.cpsr.set_v(False)
+    arm.emulate_cycle()
+    arm.emulate_cycle()
+    assert arm.registers.get(1) == BitArray(hex="0x00000013")
