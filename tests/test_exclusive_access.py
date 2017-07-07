cba699e59dcac2593d73bedc87123fa80acca68f
Matan Perelman <matan1008@gmail.com>
2017-07-07 19:03:26 +0300

    docs to test

--- tests/test_exclusive_access.py
+++ tests/test_exclusive_access.py
@@ -11,16 +11,16 @@ def test_exclusive_sanity():
     arm.registers.sctlr.set_m(False)
     arm.registers.branch_to(prev_pc)
     ram_memory = RAM(0x100)
-    ram_memory[0, 2] = "\x40\x20"
-    ram_memory[2, 2] = "\x01\x21"
-    ram_memory[4, 2] = "\x01\x60"
-    ram_memory[6, 2] = "\x00\x21"
-    ram_memory[8, 4] = "\x50\xe8\x00\x1f"
-    ram_memory[12, 4] = "\x50\xe8\x00\x2f"
-    ram_memory[16, 2] = "\x01\x31"
-    ram_memory[18, 2] = "\x02\x32"
-    ram_memory[20, 4] = "\x40\xe8\x00\x13"
-    ram_memory[24, 4] = "\x40\xe8\x00\x23"
+    ram_memory[0, 2] = "\x40\x20"  # MOVS R0, 0x40
+    ram_memory[2, 2] = "\x01\x21"  # MOVS R1, 0x1
+    ram_memory[4, 2] = "\x01\x60"  # STR R1, [R0]
+    ram_memory[6, 2] = "\x00\x21"  # MOVS R1, 0x0
+    ram_memory[8, 4] = "\x50\xe8\x00\x1f"  # LDREX R1, [R0]
+    ram_memory[12, 4] = "\x50\xe8\x00\x2f"  # LDREX R2, [R0]
+    ram_memory[16, 2] = "\x01\x31"  # ADDS R1, 0x1
+    ram_memory[18, 2] = "\x02\x32"  # ADDS R2, 0x2
+    ram_memory[20, 4] = "\x40\xe8\x00\x13"  # STREX R3, R1, [R0]
+    ram_memory[24, 4] = "\x40\xe8\x00\x23"  # STREX R3, R2, [R0]
     mc = MemoryController(ram_memory, 0x0F000000, 0x0F000100)
     arm.mem.memories.append(mc)
     arm.registers.cpsr.set_n(False)
